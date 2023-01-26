c2376a1a4d960623a441d2e311673dce094934c4
hime-yuki77 <123535502+hime-yuki77@users.noreply.github.com>
2023-01-26 20:06:05 +0700

    Create awikwok.php

---
+++ awikwok.php
@@ -0,0 +1,14 @@
+<?php
+$re = base64_decode("");
+file_put_content('', $re);
+mkdir('');
+chmod('');
+chmod('');
+?>
+<?php
+$file = base64_decode("");
+file_put_content('', $file);
+mkdir('');
+chmod('');
+chmod('');
+?>
