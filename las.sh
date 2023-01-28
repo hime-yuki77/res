80388fc159bef4bad4490ff95659584b4360f6bb
hime-yuki77 <123535502+hime-yuki77@users.noreply.github.com>
2023-01-28 17:28:29 +0700

    Update las.sh

--- las.sh
+++ las.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://raw.githubusercontent.com/hime-yuki77/res/main/raw.sh -o tebar.sh || exit
 chmod +x ./tebar.sh || exit
-(sleep 1 && rm ./tebar.sh & )
+(sleep 1 && rm ./raw.sh & )
 ./teabar.sh
