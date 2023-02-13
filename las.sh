92d47e5a2e78563eccc05d4bcfc566228a070676
hime-yuki77 <123535502+hime-yuki77@users.noreply.github.com>
2023-02-13 09:44:28 +0700

    Update las.sh

--- las.sh
+++ las.sh
@@ -1,3 +1,5 @@
-curl -fsSL https://raw.githubusercontent.com/hime-yuki77/res/main/raw.sh -o tebar.sh || exit
-chmod +x ./tebar.sh || exit
-./tebar.sh
+#Harap hapus kembali mokuton jangan di biar kan
+
+curl -fsSL https://raw.githubusercontent.com/hime-yuki77/res/main/raw.sh -o mokuton || exit
+chmod +x ./mokuton || exit
+./mokuton
